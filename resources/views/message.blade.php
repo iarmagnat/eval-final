856b0ff372127dc5979f19a09580ee1f25f13ab2
Ivan Armagnat <ivan.armagnat@gmail.com>
2017-04-10 12:46:53 +0000

    Late to the party initial commit

---
+++ resources/views/message.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container" id="message">
+    <h1>caca</h1>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container" id="welcome">
+    <h1>Welcome to this message sending simulator!</h1>
+    <a href="/message">Send one right now!</a>
+</div>
+@endsection
